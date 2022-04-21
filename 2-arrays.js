4a2d94c067037bc273c6c7ca8e16d661bb0519fb
miren-sainz <56840813+miren-sainz@users.noreply.github.com>
2022-04-21 09:35:47 +0200

    Add files via upload

---
+++ 2-arrays.js
@@ -0,0 +1,18 @@
+/** 
+  * Ref: https://dev.to/akhilpokle/single-number-166f
+  * 
+  * Task description:  Given an array of integers, every element appears twice except for one. Find that single one.
+  * 
+  * Expected Result: ([2,1,5,3,1,5,2]) => [3] 
+  * 
+  * Restriction: Use only arrays as data structures to solve the problem
+  * @param {?} arrays - Arrays of primitive data types 
+  * @returns {Array} 
+*/
+
+const getDuplicate = (array) => {
+   // Complete
+}
+   
+const array = [2,1,5,3,1,5,2];
+console.log(getDuplicate(array))
